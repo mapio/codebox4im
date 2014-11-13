f655926484a0d679739556cd465ded9d297bf8ea
Massimo Santini <santini@di.unimi.it>
2014-11-14 00:32:41 +0100

    Bumped j4im lib version

--- reprovision.sh
+++ reprovision.sh
@@ -5,7 +5,7 @@ rm -f prog.jar j4im.jar
 echo "Installing prog.jar..."
 curl -L -o prog.jar http://pighizzini.di.unimi.it/jb/librerie/prog-3ed.jar >/dev/null 2>&1
 echo "Installing j4im.jar..."
-curl -L -o j4im.jar https://github.com/mapio/j4im/releases/download/0.3-beta/j4im-0.3-beta.jar >/dev/null 2>&1
+curl -L -o j4im.jar https://github.com/mapio/j4im/releases/download/0.4-beta/j4im-0.4-beta.jar >/dev/null 2>&1
 
 chmod -R a+rX /usr/local/share/java
 
@@ -16,7 +16,7 @@ rm -rf prog j4im
 echo "Installing prog api documentation..."
 ( curl -L -o /tmp/prog.zip http://pighizzini.di.unimi.it/jb/librerie/prog-doc-3ed.zip && unzip /tmp/prog.zip && mv doc prog ) >/dev/null 2>&1
 echo "Installing j4im api documentation..."
-( curl -L -o /tmp/j4im.zip https://github.com/mapio/j4im/releases/download/0.3-beta/j4im-0.3-beta-javadoc.zip && unzip /tmp/j4im.zip && mv j4im-0.3-beta-javadoc j4im ) >/dev/null 2>&1
+( curl -L -o /tmp/j4im.zip https://github.com/mapio/j4im/releases/download/0.4-beta/j4im-0.4-beta-javadoc.zip && unzip /tmp/j4im.zip && mv j4im-0.4-beta-javadoc j4im ) >/dev/null 2>&1
 
 chmod -R a+rX /usr/share/nginx/html/docs
 
